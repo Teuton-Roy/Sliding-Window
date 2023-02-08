baaa0e4a26333314a472b6ea19af0e89757e2db1
Teuton Roy <popeyehk404@gmail.com>
2023-02-08 09:23:27 +0530

    ,.

--- Question5.java
+++ Question5.java
@@ -57,23 +57,23 @@ public class Question5 {
     }
 
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            int n;
+            System.out.println("Enter the size of array: ");
+            n = sc.nextInt();
 
-        int n;
-        System.out.println("Enter the size of array: ");
-        n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter the array elements:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
 
-        int[] arr = new int[n];
-        System.out.println("Enter the array elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            int k;
+            System.out.println("Enter the sum as k:");
+            k = sc.nextInt();
+
+            n = Longest_Subarray(arr, n, k);
+            System.out.println("Longest subarry of sum k is: "+n);
         }
-
-        int k;
-        System.out.println("Enter the sum as k:");
-        k = sc.nextInt();
-
-        n = Longest_Subarray(arr, n, k);
-        System.out.println("Longest subarry of sum k is: "+n);
     }
 }
