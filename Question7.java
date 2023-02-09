0b38e40d136babe7fb7c78b05c0c18f1a1c2f0c8
Teuton Roy <popeyehk404@gmail.com>
2023-02-09 06:46:49 +0530

    worng answer

---
+++ Question7.java
@@ -0,0 +1,64 @@
+
+
+
+
+
+/* need change */
+
+import java.util.*;
+
+public class Question7 {
+
+    public static int All_Unique_Char(String str){
+        
+        int l = str.length();
+        int start = 0;
+        int end = 0;
+        int count = -1; //if there is no unique character//
+        int window_size = end-start+1;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while(end < l){
+
+            Integer temp = map.get(str.charAt(end));
+
+            map.put(str.charAt(end), (temp==null)?1 : temp+1);
+
+            if(map.size() < window_size){
+                end++;
+            }
+
+            else if(map.size() == window_size){
+                count = Math.max(count, window_size);
+                end++;
+            }
+
+            else if(map.size() > window_size){
+                while(map.size() > window_size){
+                    map.put(str.charAt(start), map.get(str.charAt(start))-1);
+                    if(map.get(str.charAt(start))==0){
+                        map.remove(str.charAt(start));
+                    }
+                    start++;
+                }
+                end++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String str;
+        System.out.println("Enter string: ");
+        str = sc.nextLine();
+
+        // int k;
+        // System.out.println("Enter k: ");
+        // k = sc.nextInt();
+
+        int n = All_Unique_Char(str);
+        System.out.println("Longest K unique characters substring: "+n);
+    }
+}
